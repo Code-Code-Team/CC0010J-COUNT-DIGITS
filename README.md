2814dbd516d89061c707e6981bfc509b89246f4e
E2029-Aydin <aydinsanoz2@gmail.com>
2020-12-11 16:50:22 +0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # CC0010J-COUNT-DIGITS
+
+## All the numbers between number1 and number 2 are pushed into an Array in this function
+
+```bash
+function arrayOfRange(num1, num2) {
+	for (let i = num1; i <= num2; i++) {
+		const numberLength = `${i}`.length;
+		if (numberLength == 1) {
+			array.push(`${i}`);
+		} else {
+			const digitizedNum = `${i}`.split('');
+			array = array.concat(digitizedNum);
+		}
+	}
+	return array;
+}
+```
+
+## Any given value are counted in this function
+
+```bash
+function inArrayCount(val) {
+	array.forEach((element) => {
+		if (element == val) {
+			counter += 1;
+		}
+	});
+	return counter;
+}
+```
--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 const firstNumElement = document.getElementById('number1');
-console.log('🚀 ~ file: script.js ~ line 2 ~ firstNum', firstNumElement);
 const secondNumElement = document.getElementById('number2');
-console.log('🚀 ~ file: script.js ~ line 4 ~ secondNum', secondNumElement);
 const givenDigitElement = document.getElementById('digit');
-console.log('🚀 ~ file: script.js ~ line 6 ~ givenDigit', givenDigitElement);
 const resultElement = document.getElementById('result');
-console.log('🚀 ~ file: script.js ~ line 7 ~ result', resultElement);
 const buttonElement = document.getElementById('count');
-console.log('🚀 ~ file: script.js ~ line 10 ~ buttonElement', buttonElement);
 
 let array = [];
 let counter = 0;
